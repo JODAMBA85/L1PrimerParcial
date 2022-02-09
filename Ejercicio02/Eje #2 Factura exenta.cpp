6259ed928e3ef835e019639fe6230b07c4282464
JODAMBA85 <98868652+JODAMBA85@users.noreply.github.com>
2022-02-08 20:54:47 -0600

    Add files via upload

---
+++ Ejercicio02/Eje #2 Factura exenta.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ( int argc, char const *argv[] )
+int calculodelImpuesto = 0;
+
+// Entrada de datos  
+{
+    char estaExenta;
+     
+     cout << " Factura exenta? s/n ";
+      cin >> estaExenta;
+
+    if (estaExenta == 's'|| estaExenta == 's')
+    calculodelImpuesto = *0.15 ;
+}
